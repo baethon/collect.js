4c1eb90a7a2d959f1d98d485cfafda5207dc8d31
Radoslaw Mejer <radmen@desmart.com>
2016-10-17 23:34:52 +0200

    More transformation helpers

--- src/fp/transformations.js
+++ src/fp/transformations.js
@@ -1,24 +1,69 @@
 import * as R from 'ramda';
 
-const reduceIndexed = R.addIndex(R.reduce);
-
 export const collapse = R.flatten;
 
-export const combine = keys => reduceIndexed(
-  (combined, value, index) => R.assoc(keys[index], value, combined),
-  {}
+export const combine = R.zipObj;
+
+const pluckAndCombine = (valuesNames, keyName) => R.pipe(
+  R.juxt([R.pluck(keyName), R.pluck(valuesNames)]),
+  R.apply(combine)
 );
 
-export function pluck(valuesName, keyName) {
-  const withKeys = () => !!keyName;
-  const pluckValues = R.pluck(valuesName);
-  const pluckKeys = R.map(item => item[keyName]);
+export const pluck = (valuesName, keyName) => R.cond([
+  [R.always(!!keyName), pluckAndCombine(valuesName, keyName)],
+  [R.T, R.pluck(valuesName)],
+]);
 
-  return R.cond([
-    [withKeys, R.pipe(
-      R.juxt([pluckKeys, pluckValues]),
-      ([keys, values]) => combine(keys)(values)
-    )],
-    [R.T, pluckValues],
-  ]);
-}
+export const except = R.curry((exceptKeys, items) => R.pipe(
+  R.keys,
+  R.reject(R.flip(R.contains)(exceptKeys)),
+  R.reduce(
+    (prev, key) => R.assoc(key, items[key], prev),
+    {}
+  )
+)(items));
+
+export const flatMap = reducer => R.pipe(
+  R.map(reducer),
+  R.flatten,
+);
+
+const isString = value => () => R.is(String, value);
+const keyToFn = key => R.cond([
+  [isString(key), R.prop(key)],
+  [R.T, key],
+]);
+
+export const groupBy = R.curry((key, items) => R.groupBy(
+  keyToFn(key),
+  items
+));
+
+const implodeByKey = (key, glue) => R.pipe(
+  R.map(R.prop(key)),
+  R.join(glue)
+);
+
+export const implode = (key, glue) => R.cond([
+  [R.always(!!glue), implodeByKey(key, glue)],
+  [R.T, R.join(key)],
+]);
+
+export const keyBy = R.curry((key, items) => R.reduceBy(
+  (prev, current) => current,
+  {},
+  keyToFn(key),
+  items
+));
+
+export const prepend = R.prepend;
+
+export const sort = R.sort;
+
+export const sortBy = R.curry(
+  (key, items) => R.sortBy(R.prop(key), items)
+);
+
+export const reverse = R.reverse;
+
+export const unique = key => (key ? R.uniqBy(keyToFn(key)) : R.uniq);
--- test/fp/transformations.js
+++ test/fp/transformations.js
@@ -33,4 +33,81 @@ describe('transformation functions', () => {
 
     assert.deepEqual(pluckBy(items), suite.expected);
   });
+
+  runTestSuite('except')(suite => {
+    const items = suite.collection.getAll();
+    const [exceptKeys] = suite.args;
+    const except = transformations.except(exceptKeys);
+
+    assert.deepEqual(except(items), suite.expected);
+  });
+
+  runTestSuite('flatMap')(suite => {
+    const items = suite.collection.getAll();
+    const [reducerFn] = suite.args;
+    const mapper = transformations.flatMap(reducerFn);
+
+    assert.deepEqual(mapper(items), suite.expected);
+  });
+
+  runTestSuite('groupBy')(suite => {
+    const items = suite.collection.getAll();
+    const [groupByKey] = suite.args;
+    const reducer = transformations.groupBy(groupByKey);
+
+    assert.deepEqual(reducer(items), suite.expected);
+  });
+
+  runTestSuite('implode')(suite => {
+    const items = suite.collection.getAll();
+    const reducer = transformations.implode(...suite.args);
+
+    assert.deepEqual(reducer(items), suite.expected);
+  });
+
+  runTestSuite('keyBy')(suite => {
+    const items = suite.collection.getAll();
+    const [key] = suite.args;
+    const reducer = transformations.keyBy(key);
+
+    assert.deepEqual(reducer(items), suite.expected);
+  });
+
+  runTestSuite('prepend')(suite => {
+    const items = suite.collection.getAll();
+    const [value] = suite.args;
+    const prepend = transformations.prepend(value);
+
+    assert.deepEqual(prepend(items), suite.expected);
+  });
+
+  runTestSuite('sort')(suite => {
+    const items = suite.collection.getAll();
+    const comparator = suite.args && suite.args[0];
+    const sort = transformations.sort(comparator);
+
+    assert.deepEqual(sort(items), suite.expected);
+  });
+
+  runTestSuite('sortBy')(suite => {
+    const items = suite.collection.getAll();
+    const [key] = suite.args;
+    const sort = transformations.sortBy(key);
+
+    assert.deepEqual(sort(items), suite.expected);
+  });
+
+  runTestSuite('reverse')(suite => {
+    const items = suite.collection.getAll();
+
+    assert.deepEqual(transformations.reverse(items), suite.expected);
+  });
+
+  runTestSuite('unique')(suite => {
+    const items = suite.collection.getAll();
+    const args = suite.args || [];
+    const unique = transformations.unique(...args);
+
+    assert.deepEqual(unique(items), suite.expected);
+  });
 });
